da84bb66db3dc343a80af41a2afb9c63e58f1d08
hongbao <hongbaoblockchain@gmail.com>
2018-08-01 11:14:38 -0700

    first commit hb main dev

---
+++ pool/config.js
@@ -0,0 +1,19 @@
+var api = "http://52.14.17.209:8117";
+
+var api_blockexplorer = "http://52.14.17.209:11118";
+
+var poolHost = "52.14.17.209";
+
+var irc = "irc.freenode.net/#sumocoin";
+
+var email = "hongbaoblockchain@gmail.com";
+
+var cryptonatorWidget = ["XMR-BTC", "XMR-USD", "BTC-USD"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://52.14.17.209/pool/?hash={id}#blockchain_block";
+
+var transactionExplorer = "http://52.14.17.209/pool/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default-theme.css";
